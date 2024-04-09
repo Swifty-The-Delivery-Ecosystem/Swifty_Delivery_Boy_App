c8d4f31ff107403ffcada84e0088c7acea518d1c
Aditya062003 <adityavinay@iitbhilai.ac.in>
2024-04-09 22:01:49 +0530

    chore: add Location

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:swifty_delivery_boy/providers/deliveryBoyProvider.dart';
 import 'package:swifty_delivery_boy/providers/ordersProvider.dart';
@@ -24,6 +25,21 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
+    String getLocationName(int location) {
+      switch (location) {
+        case 1:
+          return 'Kanhar';
+        case 2:
+          return 'Indravati';
+        case 3:
+          return 'MSH';
+        case 4:
+          return 'Delta';
+        default:
+          return 'Unknown Location';
+      }
+    }
+
     DeliveryBoyProvider deliveryBoyProvider =
         context.read<DeliveryBoyProvider>();
     return Scaffold(
@@ -53,94 +69,118 @@ class _HomeState extends State<Home> {
                         return Card(
                           elevation: 3, // Add elevation for a subtle shadow
                           margin: EdgeInsets.all(8),
-                          color: Colors.lightBlue[50], // Light blue card color
-                          child: ListTile(
-                            title: Text(
-                              'Order ID: ${ordersProvider.orders[index].orderId}',
-                            ),
-                            subtitle: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
+                          color: Colors.white, // Light blue card color
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 15),
+                            child: ListTile(
+                              title: Row(children: [
+                                Row(children: [
+                                  Icon(Icons.location_on_outlined),
+                                  Text(
+                                    '${getLocationName(ordersProvider.orders[index].userLocation!)}',
+                                    style: GoogleFonts.voltaire(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ]),
+                                Spacer(),
                                 Text(
-                                  'Order Amount: ${ordersProvider.orders[index].amount}',
+                                  'Rs ${ordersProvider.orders[index].amount}',
                                   style: TextStyle(
                                     fontWeight: FontWeight.bold,
                                   ),
                                 ),
-                                SizedBox(height: 8),
-                                Text(
-                                  'Delivery Items:',
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                ),
-                                // Displaying delivery items
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: ordersProvider.orders[index].items
-                                      .map((item) => Text(
-                                          '${item.name} x ${item.quantity}'))
-                                      .toList(),
-                                ),
-                                SizedBox(height: 8),
-                                Text('Update Order Status:'),
-                                DropdownButton<String>(
-                                  value:
-                                      ordersProvider.orders[index].orderStatus,
-                                  onChanged: (String? newValue) async {
-                                    if (newValue != null) {
-                                      ordersProvider.orders[index].orderStatus =
-                                          newValue;
-                                      ordersProvider.notifyListeners();
+                              ]),
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Order ID: ${ordersProvider.orders[index].orderId}',
+                                  ),
 
-                                      if (newValue == 'Arrived' ||
-                                          newValue == 'Delivered') {
-                                        String orderId = ordersProvider
-                                            .orders[index].orderId;
-                                        String token =
-                                            Provider.of<DeliveryBoyProvider>(
-                                                    context,
-                                                    listen: false)
-                                                .token;
+                                  SizedBox(height: 8),
+                                  Text(
+                                    'Delivery Items:',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  // Displaying delivery items
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: ordersProvider.orders[index].items
+                                        .map((item) => Text(
+                                            '${item.name} x ${item.quantity}'))
+                                        .toList(),
+                                  ),
+                                  SizedBox(height: 8),
+                                  Text('Update Order Status:'),
+                                  DropdownButton<String>(
+                                    value: ordersProvider
+                                        .orders[index].orderStatus,
+                                    onChanged: (String? newValue) async {
+                                      if (newValue != null) {
+                                        ordersProvider.orders[index]
+                                            .orderStatus = newValue;
+                                        ordersProvider.notifyListeners();
 
-                                        // Call the UpdateOrderProvider to update the order status
-                                        int responseCode = await Provider.of<
-                                                    UpdateOrderProvider>(
-                                                context,
-                                                listen: false)
-                                            .updateOrder(orderId,
-                                                newValue.toLowerCase(), token);
+                                        if (newValue == 'Arrived' ||
+                                            newValue == 'Delivered') {
+                                          String orderId = ordersProvider
+                                              .orders[index].orderId;
+                                          String token =
+                                              Provider.of<DeliveryBoyProvider>(
+                                                      context,
+                                                      listen: false)
+                                                  .token;
 
-                                        // Show Snackbar based on the response code
-                                        ScaffoldMessenger.of(context)
-                                            .showSnackBar(
-                                          SnackBar(
-                                            content: Text(responseCode == 200 ||
-                                                    responseCode == 201
-                                                ? 'Order Updated Successfully'
-                                                : 'Error updating order: $responseCode'),
-                                          ),
-                                        );
+                                          // Call the UpdateOrderProvider to update the order status
+                                          int responseCode = await Provider.of<
+                                                      UpdateOrderProvider>(
+                                                  context,
+                                                  listen: false)
+                                              .updateOrder(
+                                                  orderId,
+                                                  newValue.toLowerCase(),
+                                                  token);
 
-                                        // If the order is delivered, remove it from the list
-                                        if (newValue == 'Delivered') {
-                                          ordersProvider.orders.removeAt(index);
-                                          ordersProvider.notifyListeners();
+                                          // Show Snackbar based on the response code
+                                          ScaffoldMessenger.of(context)
+                                              .showSnackBar(
+                                            SnackBar(
+                                              content: Text(responseCode ==
+                                                          200 ||
+                                                      responseCode == 201
+                                                  ? 'Order Updated Successfully'
+                                                  : 'Error updating order: $responseCode'),
+                                            ),
+                                          );
+
+                                          // If the order is delivered, remove it from the list
+                                          if (newValue == 'Delivered') {
+                                            ordersProvider.orders
+                                                .removeAt(index);
+                                            ordersProvider.notifyListeners();
+                                          }
                                         }
                                       }
-                                    }
-                                  },
-                                  items: ordersProvider
-                                      .getAvailableOrderStatuses(ordersProvider
-                                          .orders[index].orderStatus)
-                                      .map((String value) {
-                                    return DropdownMenuItem<String>(
-                                      value: value,
-                                      child: Text(value),
-                                    );
-                                  }).toList(),
-                                ),
-                              ],
+                                    },
+                                    items: ordersProvider
+                                        .getAvailableOrderStatuses(
+                                            ordersProvider
+                                                .orders[index].orderStatus)
+                                        .map((String value) {
+                                      return DropdownMenuItem<String>(
+                                        value: value,
+                                        child: Text(value),
+                                      );
+                                    }).toList(),
+                                  ),
+                                ],
+                              ),
+                              // Add more details as needed
                             ),
-                            // Add more details as needed
                           ),
                         );
                       },
